33e0e2d5307d2e021fc564c9f1102ca45b8cfff3
yeonkyung <younkyong94@gmail.com>
2022-11-02 17:32:51 +0900

    init

--- src/main/java/com/example/demo/core/AppConfig.java
+++ src/main/java/com/example/demo/core/AppConfig.java
@@ -8,21 +8,28 @@ import com.example.demo.core.member.MemberServiceImpl;
 import com.example.demo.core.member.MemoryMemberRepository;
 import com.example.demo.core.order.OrderService;
 import com.example.demo.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new FixDiscountPolicy();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(
                 memberRepository(),
